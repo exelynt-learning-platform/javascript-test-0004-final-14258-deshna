b7f8f0312e2631e2e25ddb28ae7be3e40e2a9bad
desshna <jdeshna.05@gmail.com>
2026-03-14 12:41:16 +0530

    Add files via upload

---
+++ squarePattern.js
@@ -0,0 +1,10 @@
+let n = 4;
+
+for (let i = 0; i < 2 * n - 1; i++) {
+    let row = "";
+    for (let j = 0; j < 2 * n - 1; j++) {
+        let minDist = Math.min(i, j, (2 * n - 2) - i, (2 * n - 2) - j);
+        row += (n - minDist) + " ";
+    }
+    console.log(row.trim());
+}
